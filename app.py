5cc0561d959bee415139f458faee64ea6daacb0c
kaifcoder <kaifmohd2014@gmail.com>
2025-02-14 10:48:28 +0530

    feat: add main entry point to run the application with Uvicorn

--- app.py
+++ app.py
@@ -13,3 +13,7 @@ def read_item(item_id: int, q: str = None):
 @app.get("/hey/{name}")
 def say_hey(name: str):
     return {"message": "Hey, " + name}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
