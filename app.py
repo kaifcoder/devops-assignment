25ccb6bf88f5ab975f14c1592191ca454d9e42b7
kaifcoder <kaifmohd2014@gmail.com>
2025-03-10 11:36:48 +0530

    refactor: remove unused routes and clean up app.py [skip ci]

--- app.py
+++ app.py
@@ -6,27 +6,9 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
-
-@app.get("/hey/{name}")
-def say_hey(name: str):
-    return {"message": "Hey, " + name}
-
 @app.get("/hello/{name}")
 def say_hello(name: str):
     return {"message": "Hello, " + name}
-
-# break the code
-@app.get("/error")
-def error():
-    return 1/0
-
-@app.get("/experiment}")
-def say_hello(name: str):
-    return {"message": "mistake"}
-
 
 if __name__ == "__main__":
     import uvicorn
