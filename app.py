ceeee90ea03a103373ccf8828a40146727110c0d
kaifcoder <kaifmohd2014@gmail.com>
2025-03-13 00:30:20 +0530

    Add power endpoint to calculate exponentiation

--- app.py
+++ app.py
@@ -28,6 +28,11 @@ def divide(a: int, b: int):
 def subtract(a: int, b: int):
     return {"result": a - b}
 
+@app.get("/power/{a}/{b}")
+def power(a: int, b: int):
+    a = 
+    return {"result": a ** b}
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
