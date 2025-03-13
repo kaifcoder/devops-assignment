78c24ed460ba6cc94b763bdcedf2a62ef843534f
kaifcoder <kaifmohd2014@gmail.com>
2025-03-13 13:37:22 +0530

    Refactor /add endpoint to remove unused parameters and simplify response structure [skip ci]

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI
 
 app = FastAPI()
 
@@ -10,27 +10,8 @@ def read_root():
 def say_hello(name: str):
     return {"message": "Hello, " + name}
 
-
-'''
-    ENDPOINT: /add/{a}/{b}
-    PARAMS:
-        a: int
-        b: int
-        new: bool
-    RETURNS:
-        {
-            "result": int
-        }
-    DESCRIPTION:
-        Returns the sum of a and b.
-        If the new key is set to True, the key will be "sum" instead of "result".
-'''
 @app.get("/add/{a}/{b}")
-def add(a: int, b: int, new: bool = Query(False,alias="new")):
-    # if user wants to use the new key, return the sum with the key "sum" instead of "result"
-    if new:
-        return {"sum": a + b}
-    # otherwise, return the sum with the key "result" as usual
+def add(a: int, b: int):
     return {"result": a + b}
 
 @app.get("/multiply/{a}/{b}")
